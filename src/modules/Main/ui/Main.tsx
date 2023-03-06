52ed47784aa4109847f4b003a55344d4aab6a63f
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-06 16:29:56 +0200

    final

---
+++ src/modules/Main/ui/Main.tsx
@@ -0,0 +1,39 @@
+import styles from "./Main.module.css"
+//@ts-ignore
+import video from "assets/video/video.mp4"
+import { Button, ButtonVariant } from "modules/common/Button"
+
+export function Main() {
+    return (
+        <div className={styles.wrapper}>
+            <video
+                src={video}
+                //@ts-ignore
+                type="video/mp4"
+                loop
+                controls={false}
+                muted
+                playsInline
+                autoPlay={false}
+                className={styles.video}
+            />
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>FLAWLESS</h1>
+                    <p className={styles.description}>
+                        An all-encompassing nightlife experience with a variety
+                        of atmospheres and world-renowned musical talent.
+                    </p>
+                    <div className={styles.btnContainer}>
+                        <Button variant={ButtonVariant.FILLED}>
+                            BUY TICKETS
+                        </Button>
+                        <Button variant={ButtonVariant.OUTLINED}>
+                            BOOK TABLE
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
