e4a79c8acc2d234c0862512af813dc5131e9a853
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-06 17:27:56 +0200

    delete unused

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { SocialIcons } from "modules/common/components/SocialIcons"
 import { Main } from "modules/Main"
 import "./index.css"
 
@@ -6,7 +5,6 @@ export function App() {
     return (
         <div>
             <Main />
-            <SocialIcons />
         </div>
     )
 }
