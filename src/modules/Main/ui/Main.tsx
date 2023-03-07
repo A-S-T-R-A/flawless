305220b34d61313e002a5d2f7ec240134f56aadc
Art1us <109896229+Art1us@users.noreply.github.com>
2023-03-07 15:54:11 +0200

    add minHeight

--- src/modules/Main/ui/Main.tsx
+++ src/modules/Main/ui/Main.tsx
@@ -3,9 +3,11 @@ import styles from "./Main.module.css"
 import video from "assets/video/video.mp4"
 import { Button, ButtonVariant } from "modules/common/Button"
 
+const minHeight = window.innerHeight
+
 export function Main() {
     return (
-        <div className={styles.wrapper}>
+        <div className={styles.wrapper} style={{ minHeight }}>
             <video
                 src={video}
                 //@ts-ignore
