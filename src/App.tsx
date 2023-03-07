703a91eec6e77a946dd8273d7d27e44d1f6eaa62
je-delray <hovax74935@ekcsoft.com>
2023-03-07 10:04:30 +0100

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Main } from "modules/Main"
 import "./index.css"
+import { Gallery } from "./modules/Gallery"
 import { Footer } from "./modules/Footer"
 
 export function App() {
     return (
         <div>
             <Main />
+            <Gallery />
             <Footer />
         </div>
     )
