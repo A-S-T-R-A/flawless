b616713e1f4010ebc6b728a66a3ebb3df0616b9c
je-delray <hovax74935@ekcsoft.com>
2023-03-08 14:38:46 +0100

    logo

--- src/modules/Navigation/Navigation.js
+++ src/modules/Navigation/Navigation.js
@@ -3,6 +3,8 @@ import styles from "./Navigation.module.css"
 import { Burger } from "./components/Burger"
 import { Navbar } from "./components/Navbar"
 import { classNames } from "modules/common/helpers/classNames"
+import logo from "assets/images/logo.png"
+// import logowhite from "assets/images/logowhite.png"
 
 export function Navigation() {
     const [navbarVisible, setNavbarVisible] = useState(false)
@@ -27,7 +29,7 @@ export function Navigation() {
     return (
         <div className={wrapperClassName}>
             <div className={styles.container}>
-                <div className={styles.logoContainer}>FLAWLESS</div>
+                <img className={styles.logo} src={logo} alt="logo" />
                 <Navbar />
                 <Burger />
             </div>
