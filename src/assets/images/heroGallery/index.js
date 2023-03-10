2bb3266f07dae4b9ad9941915987f413eaf6d4d3
je-delray <hovax74935@ekcsoft.com>
2023-03-10 15:26:01 +0100

    Merge branch 'feature/13.hero-gallery' of https://github.com/A-S-T-R-A/flawless into feature/13.hero-gallery

--- src/assets/images/heroGallery/index.js
+++ src/assets/images/heroGallery/index.js
@@ -1,12 +1,11 @@
-import firstImg from "./1.jpg"
-import secondImg from "./2.jpg"
-import thirdImg from "./3.jpg"
-import fourthImg from "./4.jpg"
-import fifthImg from "./5.jpg"
-import sixthImg from "./6.jpg"
-import seventhImg from "./7.jpg"
-import eightsImg from "./8.jpg"
-import ninethImg from "./9.jpg"
+import firstImg from "./1.jpeg"
+import secondImg from "./2.jpeg"
+import thirdImg from "./3.jpeg"
+import fourthImg from "./4.jpeg"
+import fifthImg from "./5.jpeg"
+import sixthImg from "./6.jpeg"
+import seventhImg from "./7.jpeg"
+import eightsImg from "./8.jpeg"
 
 export {
     firstImg,
@@ -17,5 +16,4 @@ export {
     sixthImg,
     seventhImg,
     eightsImg,
-    ninethImg,
 }
