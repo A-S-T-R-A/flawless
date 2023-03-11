37d9c82a44ead06f4f9a9510da2637dda3cf3638
VadimGalian <vadim.galian@gmail.com>
2023-03-11 17:49:55 +0200

    inter

--- src/modules/common/components/VideoBg/VideoBg.js
+++ src/modules/common/components/VideoBg/VideoBg.js
@@ -1,18 +1,37 @@
+import { useState } from "react"
 import video from "assets/video/video.mp4"
+import videoPlaceHolder from "assets/images/main/first.webp"
 import styles from "./VideoBg.module.css"
 
-export function VideoBg() {
+export default function VideoBg() {
+    const [showVideo, setShowVideo] = useState(false)
+
+    function handleLoad() {
+        setTimeout(() => {
+            setShowVideo(true)
+        }, 5000)
+    }
+
     return (
-        <video
-            src={video}
-            //@ts-ignore
-            type="video/mp4"
-            loop
-            controls={false}
-            muted
-            playsInline
-            autoPlay={true}
-            className={styles.video}
-        />
+        <>
+            <img
+                src={videoPlaceHolder}
+                alt="videoPlaceHolder"
+                onLoad={handleLoad}
+            />
+            {showVideo && (
+                <video
+                    src={video}
+                    //@ts-ignore
+                    type="video/mp4"
+                    loop
+                    controls={false}
+                    muted
+                    playsInline
+                    autoPlay={true}
+                    className={styles.video}
+                />
+            )}
+        </>
     )
 }
