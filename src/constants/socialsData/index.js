b25ac4bd9f61f257dd449e833d93032cc4121dd5
je-delray <hovax74935@ekcsoft.com>
2023-03-11 14:50:20 +0100

    - alts

--- src/constants/socialsData/index.js
+++ src/constants/socialsData/index.js
@@ -6,18 +6,15 @@ export const socialsData = [
         id: 1,
         link: "https://web.telegram.org/z/#-1255295281",
         icon: <FaTelegramPlane />,
-        alt: "Telegram",
     },
     {
         id: 2,
         link: "https://www.instagram.com/flawlessldynasty/",
         icon: <FaInstagram />,
-        alt: "Instagram",
     },
     {
         id: 3,
         link: "https://www.whatsapp.com/",
         icon: <MdWhatsapp />,
-        alt: "WhatsApp",
     },
 ]
